577287e45736aafe5304bd1ac8bf4ccb51706c2d
ALISON SALINAS <gisellsalinas43@gmail.com>
2023-08-21 23:34:28 -0400

    created card and test

--- src/GitHubCard.js
+++ src/GitHubCard.js
@@ -1,8 +1,21 @@
 import React from 'react'
+import Card from 'react-bootstrap/Card'
+import Button from 'react-bootstrap/Button'
 
 function GitHubCard() {
   return (
-    <div>GitHubCard</div>
+    <div>
+        <Card style={{ width: '18rem' }}>
+      <Card.Img variant="top" src="https://avatars.githubusercontent.com/u/126828743?v=4" />
+      <Card.Body>
+        <Card.Title>GitHub Profile: BICHOTALI</Card.Title>
+        <Card.Text>
+          I LOVE CODING!
+        </Card.Text>
+        <Button variant="primary">Go Visit!</Button>
+      </Card.Body>
+    </Card>
+    </div>
   )
 }
 
